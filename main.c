177ac07564f68953bd1f774f4e3f8b34cfecb34b
SzkopikRafal <245936@edu.p.lodz.pl>
2022-11-06 21:44:43 +0100

    02 Zmiana mnozenia w metodzie policz

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int policz(int a,int b)
+{
+    return a*b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    printf("w drugim branchu: 2*3=%d", policz(2,3));
     return 0;
 }
